7cb06c4fc9612c22e19377e561b7dbf487d24117
goodGid <hello.goodgid@gmail.com>
2022-08-12 01:04:02 +0900

    `MDC`를 사용한 logback 설정

---
+++ src/main/java/dev/be/logback/controller/MdcController.java
@@ -0,0 +1,28 @@
+package dev.be.logback.controller;
+
+import org.slf4j.MDC;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class MdcController {
+
+    @GetMapping("/mdc")
+    public String mdc() {
+
+        MDC.put("job", "dev");
+
+        log.trace("log --> TRACE");
+        log.debug("log --> DEBUG");
+        log.info("log --> INFO");
+        log.warn("log --> WARN");
+        log.error("log --> ERROR");
+
+        MDC.clear();
+
+        return "mdc";
+    }
+}
